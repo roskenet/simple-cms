66618c254a4aaea95b4b4831ac5ddeb57b604313
Felix Roske <f.roske@roskenet.de>
2015-10-04 17:40:43 +0200

    Change default server port. Answer with landing page on index or index.html

--- src/main/java/de/roskenet/simplecms/PageController.java
+++ src/main/java/de/roskenet/simplecms/PageController.java
@@ -1,7 +1,11 @@
 package de.roskenet.simplecms;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -14,17 +18,24 @@ public class PageController {
 	
 	@Value("${spring.thymeleaf.prefix}")
 	private String prefix;
-	
-	@RequestMapping("/{template}/{content}")
-	public String greeting(@PathVariable("template") String template, @PathVariable("content") String content, Model model) {
-		Map<String,Object> hashMap = new HashMap<String, Object>();
-		
-		System.out.println("Prefix: " + prefix);
-		System.out.println("Template: " + template);
-		
-		hashMap.put("STATIC", "https://felixroskede.s3.eu-central-1.amazonaws.com");
-		hashMap.put("CONTENT", "/content/" + content);
-		model.addAllAttributes(hashMap);
-		return "tmpl/" + template;
+
+	@RequestMapping("/")
+	public String root() {
+		return "index";
 	}
+	
+	@RequestMapping("/{page}")
+	public String greeting(@PathVariable("page") String content, Model model, HttpSession session) {
+
+		if(content.equals("index") || content.equals("index.html")) {
+			return "index";
+		}
+		
+		Map<String, Object> hashMap = new HashMap<String, Object>();
+		hashMap.put("CONTENT", content);
+		hashMap.put("STATIC", "https://s3.eu-central-1.amazonaws.com/felixroskede/");
+		model.addAllAttributes(hashMap);
+		return "page";
+	}
+	
 }
