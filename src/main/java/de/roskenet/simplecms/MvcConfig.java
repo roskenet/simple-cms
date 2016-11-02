1ef245c17a2133e5a563d1d9c3a9ba9c22fcf2d2
Felix Roske <f.roske@roskenet.de>
2016-11-02 18:54:37 +0100

    Correct static path to file

--- src/main/java/de/roskenet/simplecms/MvcConfig.java
+++ src/main/java/de/roskenet/simplecms/MvcConfig.java
@@ -1,15 +1,24 @@
 package de.roskenet.simplecms;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
+	
+	@Value("${spring.thymeleaf.prefix}")
+	private String prefix;
+	
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
           .addResourceHandler("/static/**")
-          .addResourceLocations("file:/srv/apache2/cms/static/");    
+          .addResourceLocations(staticDir());    
+    }
+    
+    private String staticDir() {
+    	return new StringBuilder().append(prefix).append("static/").toString();
     }
 }
