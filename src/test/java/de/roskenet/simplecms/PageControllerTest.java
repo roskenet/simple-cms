0d341c21b3aef88043273457391c49d71e3f9e55
Felix Roske <f.roske@roskenet.de>
2016-11-03 20:17:19 +0100

    Add a test

---
+++ src/test/java/de/roskenet/simplecms/PageControllerTest.java
@@ -0,0 +1,15 @@
+package de.roskenet.simplecms;
+
+import org.junit.Test;
+
+import de.roskenet.simplecms.controller.PageController;
+
+public class PageControllerTest {
+
+	private PageController pageCtrl = new PageController();
+	
+	
+	public void returnPage() {
+
+	}
+}
